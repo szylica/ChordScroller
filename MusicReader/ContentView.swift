00c403a2d16c72896f084ff886fb52ab223aff4f
szylica <szymonstalica414@gmail.com>
2026-06-06 18:27:13 +0200

    Initial Commit

---
+++ MusicReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicReader
+//
+//  Created by Szymon Stalica on 06/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicReader/MusicReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicReaderApp.swift
+//  MusicReader
+//
+//  Created by Szymon Stalica on 06/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MusicReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
